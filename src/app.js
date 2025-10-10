d5ac2717ab0059025fbf054e6d6e1925ad5002a3
LAPTOP-2P8M32F9\Archana Yadav <archanaarchana1992@gmail.com>
2025-10-11 00:56:58 +0530

    routing

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const app =express();
 
-app.use("/",(req,res)=>{
-    res.send("hello world  ss")
+app.use('/test/2',(req,res)=>{
+    res.send("test2")
 })
 
 app.use('/test',(req,res)=>{
@@ -11,3 +11,22 @@ app.use('/test',(req,res)=>{
 app.listen(3000,()=>{
     console.log("listening at 3000")
 })
+
+app.get('/user',(req,res)=>{
+    res.send("User data")
+})
+
+app.post('/user',(req,res)=>{
+    res.send("data saved")
+})
+
+app.delete('/user',(req,res)=>{
+    res.send("delete")
+})
+
+app.put('/user',(req,res)=>{
+    res.send("put call")
+})
+// app.use("/",(req,res)=>{
+//     res.send("hello world  ss")
+// })
