2857bb41e037e5c9b9a61507feba3b8048ff724a
LAPTOP-2P8M32F9\Archana Yadav <archanaarchana1992@gmail.com>
2025-10-11 01:48:13 +0530

    multiple route handler

--- src/app.js
+++ src/app.js
@@ -1,32 +1,27 @@
 const express = require('express')
 const app =express();
 
-app.use('/test/2',(req,res)=>{
-    res.send("test2")
-})
-
-app.use('/test',(req,res)=>{
-    res.send("test")
-})
 app.listen(3000,()=>{
     console.log("listening at 3000")
 })
 
-app.get('/user',(req,res)=>{
-    res.send("User data")
-})
+app.use('/user',[(req,res,next)=>{
+   console.log("Handling user 1")
+    next()
+   // res.send("send 1")
+    
+},(req,res,next)=>{
+    console.log("Handling user 2")
+    //res.send("send 2")
+    next()
+},
+(req,res,next)=>{
+    console.log("Handling user 2")
+    //res.send("send 3")
+    next()
+},
+(req,res)=>{
+    console.log("Handling user 2")
+    res.send("send 4")
+}])
 
-app.post('/user',(req,res)=>{
-    res.send("data saved")
-})
-
-app.delete('/user',(req,res)=>{
-    res.send("delete")
-})
-
-app.put('/user',(req,res)=>{
-    res.send("put call")
-})
-// app.use("/",(req,res)=>{
-//     res.send("hello world  ss")
-// })
